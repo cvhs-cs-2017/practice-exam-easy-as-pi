c4efd6f0bdb8c5e197f8203b16b91fd1aa49610d
easy-as-pi <mohanpr092201@cvh.rusd.us>
2017-01-23 10:37:39 -0800

    Range Function completed!

--- Range.Function.py
+++ Range.Function.py
@@ -1,9 +1,14 @@
 """Use the range function to print the numbers from 1-20"""
 
-
+for i in range(1,21):
+    print(i)
 
 """Repeat the exercise above counting by 2's"""
-
-
+print('')
+for i in range(0,21,2):
+    print(i)
 
 """Print all the multiples of 5 between 10 and 200 in DECENDING order"""
+print('')
+for i in range(200,9,-5):
+    print(i)
