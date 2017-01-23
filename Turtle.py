3fb9c4faadfc0a3450f525472c0cec0108d08711
easy-as-pi <mohanpr092201@cvh.rusd.us>
2017-01-23 10:46:01 -0800

    Turtle Completed!

--- Turtle.py
+++ Turtle.py
@@ -1,1 +1,17 @@
 """ Create a Turtle, name it, make it BLUE and draw a Smiley Face"""
+
+import turtle
+p = turtle.Turtle()
+p.color("Blue")
+p.right(90)
+p.fd(50)
+p.pu()
+p.goto(25,0)
+p.pd()
+p.fd(50)
+p.pu()
+p.goto(-25,-50)
+p.pd()
+for i in range(20):
+    p.fd(6)
+    p.left(10)
