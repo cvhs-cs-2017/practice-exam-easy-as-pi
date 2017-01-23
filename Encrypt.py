f7e63a42496a2f6c54aa04f37ce6b0f75b01b06d
easy-as-pi <mohanpr092201@cvh.rusd.us>
2017-01-23 10:26:25 -0800

    Encrypt Done!

--- Encrypt.py
+++ Encrypt.py
@@ -1,2 +1,28 @@
 """Write a code that will take (AnyString) and substitute all vowels with
 character's like $#%^&* etc..... but DO NOT USE ' or " as any of your characters."""
+
+def SubVowels(AnyString):
+    CipherText = ""
+    CipherSub = "$#%^&"
+    anyString = AnyString.lower()
+    for ch in AnyString:
+        if ch == "a":
+            ch = CipherSub[0]
+            CipherText += ch
+        elif ch == "e":
+            ch = CipherSub[1]
+            CipherText += ch
+        elif ch == "i":
+            ch = CipherSub[2]
+            CipherText += ch
+        elif ch == "o":
+            ch = CipherSub[3]
+            CipherText += ch
+        elif ch == "u":
+            ch = CipherSub[4]
+            CipherText += ch
+        else:
+            CipherText += ch
+    return CipherText
+
+print(SubVowels("The Quick Brown Fox Jumped Over The Lazy Dog."))
